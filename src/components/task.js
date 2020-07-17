f850f1497f95037c8c33c35f97962a2ce073e419
Bulat <sadikovba@gmail.com>
2020-07-17 16:25:21 +0300

    5.2.7 Перенесет файл `utils.js` в `utils/common.js`

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,6 @@
 import AbstrctComponent from "./abstract-component.js";
 import { MONTH_NAMES } from "../const.js";
-import { formatTime } from "../utils.js";
+import { formatTime } from "../utils/common.js";
 
 // Функцию для генерации HTML-разметки можно превратить в метод класса,
 // однако делать мы этого не будем, потому что это не критично,
