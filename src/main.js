666cbc1745746aa80ebe95fd5e633955963b5a8c
Bulat <sadikovba@gmail.com>
2020-08-12 11:25:07 +0300

    7.2.3 Опишет функцию переключения экранов

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
 import FilterController from "./controllers/filters.js";
+import StatisticComponent from "./components/statistics.js";
 import SiteMenuComponent, { MenuItem } from "./components/site-menu.js";
 import TasksModel from "./models/tasks.js";
 import { generateTasks } from "./mock/task.js";
@@ -27,11 +28,35 @@ render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 const boardController = new BoardController(boardComponent, tasksModel);
 boardController.render();
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+const statisticComponent = new StatisticComponent({
+  tasks: tasksModel,
+  dateFrom,
+  dateTo,
+});
+render(siteMainElement, statisticComponent, RenderPosition.BEFOREEND);
+statisticComponent.hide();
+
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       SiteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticComponent.hide();
+      boardComponent.show();
       break;
   }
 });
