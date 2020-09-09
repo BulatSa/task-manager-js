db038c57610898b18993633d70ca229ab894e0c5
Bulat <sadikovba@gmail.com>
2020-09-09 20:54:40 +0300

    9.1.6 Добавит реализацию `Provider#updateTask`

--- src/api/provider.js
+++ src/api/provider.js
@@ -13,12 +13,11 @@ export default class Provider {
 
   getTasks() {
     if (isOnline()) {
-      return this._api.getTasks()
-        .then((tasks) => {
-          tasks.forEach((task) => this._store.setItem(task.id, task.toRAW()));
+      return this._api.getTasks().then((tasks) => {
+        tasks.forEach((task) => this._store.setItem(task.id, task.toRAW()));
 
-          return tasks;
-        });
+        return tasks;
+      });
     }
 
     const storeTasks = Object.values(this._store.getItems());
@@ -36,10 +35,18 @@ export default class Provider {
 
   updateTask(id, task) {
     if (isOnline()) {
-      return this._api.updateTask(id, task);
+      return this._api.updateTask(id, task).then((newTask) => {
+        this._store.setItem(newTask.id, newTask.toRAW());
+
+        return newTask;
+      });
     }
 
-    return Promise.reject(`offline`);
+    const localTask = Task.clone(Object.assign(task, { id }));
+
+    this._store.setItem(id, localTask.toRAW());
+
+    return Promise.resolve(localTask);
   }
 
   deleteTask(id) {
